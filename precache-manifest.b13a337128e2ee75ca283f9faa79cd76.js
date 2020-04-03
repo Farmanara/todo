9b234d3b26346506f18361ef6eace053b7c720bc
Amir <farmanara.code@gmail.com>
2020-04-04 03:48:27 +0430

    Updates

---
+++ precache-manifest.b13a337128e2ee75ca283f9faa79cd76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bf59b8b0832ce1473347b5393bd2486",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "50ace478ee8236c155d4",
+    "url": "/todo/static/css/main.c505cf88.chunk.css"
+  },
+  {
+    "revision": "5b3e2b6ab637cd7a511c",
+    "url": "/todo/static/js/2.c6f55bef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.c6f55bef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50ace478ee8236c155d4",
+    "url": "/todo/static/js/main.c5e2a619.chunk.js"
+  },
+  {
+    "revision": "0fad5e3480eda5bbef65",
+    "url": "/todo/static/js/runtime-main.64f6f58d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
